cabb6a64022733a7585a98db56c006b9c45fc161
gabriwls <gabriwlalves@hotmail.com>
2022-08-02 15:13:11 -0300

    cria o layout de adição de novo cartão de visita

---
+++ app/src/main/java/com/gabriwls/portanegocios/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,18 @@
+package com.gabriwls.portanegocios.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabriwls.portanegocios.R
+import com.gabriwls.portanegocios.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityAddBusinessCardBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
